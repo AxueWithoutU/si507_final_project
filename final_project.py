f08a5ed816f072f22b0a995aad53fe8fe332239e
AxueWithoutU <iceriawind@gmail.com>
2023-12-14 23:01:08 -0500

    Created separate functions for making combined figures

--- final_project.py
+++ final_project.py
@@ -1,8 +1,10 @@
 import networkx as nx
 import tkinter as tk
 from tkinter import ttk
+import math
 import matplotlib.pyplot as plt
 import json
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import textwrap
 import copy
 import os
@@ -20,14 +22,34 @@ Enter 1 to choose r/Obesity\nEnter 2 to choose r/Fatlogic\n\nChoose a subreddit:
         layout = input("""\nNow, please choose the layout format for your image(s).\n
 Enter 1 to choose the Kamada_Kawai layout\nEnter 2 to choose the shell layout
 Enter 3 to choose the spring layout\nEnter 4 to choose the spiral layout
-Enter 5 to choose the planar layout\n\nChoose a layout: """)
+Enter 5 to choose the planar layout\n\nAny other input values will yield a planar layout. Choose a layout: """)
         compInput = input("""\nFinally, please enter 1 to generate a single graph from a random comment tree, and
 enter 2 if you wish to generate graphs for all of the comment trees: """)
         if compInput == '1':
             plotObesityGraph(posts_with_comments[random.randint(0, len(posts_with_comments))], layout)
         elif compInput == '2':
-            for p in posts_with_comments:
-                plotObesityGraph(p, layout)
+            # Calculate the number of rows and columns
+            num_plots = len(posts_with_comments)
+            num_cols = math.ceil(math.sqrt(num_plots))
+            num_rows = math.ceil(num_plots / num_cols)
+
+            # Create a figure with multiple subplots
+            fig, axs = plt.subplots(num_rows, num_cols, figsize=(200, 150))
+
+            # Plot each graph onto a subplot
+            for i, p in enumerate(posts_with_comments):
+                row = i // num_cols  # get floor for row
+                col = i % num_cols
+                plotObesityMany(p, axs[row, col], layout)
+
+            # Remove empty subplot(s)
+            for i in range(num_plots, num_rows * num_cols):
+                fig.delaxes(axs.flatten()[i])
+
+            # Adjust layout and display the combined figure
+            plt.subplots_adjust(wspace=0.3, hspace=0.5)  # Adjust spacing between subplots
+            plt.tight_layout()
+            plt.show()
         else:
             print('Please follow the directions and enter a viable value')
     # generate for r/Fatlogic
@@ -35,14 +57,24 @@ enter 2 if you wish to generate graphs for all of the comment trees: """)
         layout = input("""\nNow, please choose the layout format for your image(s).\n
 Enter 1 to choose the Kamada_Kawai layout\nEnter 2 to choose the shell layout
 Enter 3 to choose the spring layout\nEnter 4 to choose the spiral layout
-Enter 5 to choose the planar layout\n\nChoose a layout: """)
+Enter 5 to choose the planar layout\n\nAny other input values will yield a planar layout. Choose a layout: """)
         compInput = input("""\nFinally, please enter 1 to generate a single graph from a random comment tree, and
 enter 2 if you wish to generate graphs for all of the comment trees: """)
         if compInput == '1':
-            plotFatGraph(fl_data[random.randint(0, len(posts_with_comments))], layout)
+            plotFatGraph(fl_data[random.randint(0, len(fl_data))], layout)
         elif compInput == '2':
-            for p in fl_data:
-                plotFatGraph(p, layout)
+            num_plots = len(fl_data)
+            num_cols = math.ceil(math.sqrt(num_plots))
+            num_rows = math.ceil(num_plots / num_cols)
+            fig, axs = plt.subplots(num_rows, num_cols, figsize=(8, 6))
+            for i, p in enumerate(fl_data):
+                row = i // num_cols  # get floor
+                col = i % num_cols   
+                plotFatMany(p, axs[row, col], layout)
+            for i in range(num_plots, num_rows * num_cols):
+                fig.delaxes(axs.flatten()[i])
+            plt.tight_layout()
+            plt.show()
         else:
             print('Please follow the directions and enter a viable value')
     else:
@@ -123,13 +155,13 @@ def addNodesFat(G, comment):
         G.add_edge(comment["comment_id"], reply["comment_id"])
         addNodesFat(G, reply)
 
-def plotObesityGraph(post_data, layout=1):
+def plotObesityGraph(post_data, layout=5):
     '''plot a graph based on a comment tree for a post in r/obesity
     '''
     G = nx.DiGraph()
     # add root node for the post
     root_id = post_data["submission_id"]
-    G.add_node(root_id, label=root_id, color="palevioletred")  # set root node color to red
+    G.add_node(root_id, label=root_id, color="palevioletred")  # set root node color
     # add edges from the post to top-level comments
     for comment in post_data.get("comments", []):
         # set color for nodes surrounding root node
@@ -142,6 +174,7 @@ def plotObesityGraph(post_data, layout=1):
     node_colors = [G.nodes[node]["color"] for node in G.nodes]
     # get node labels based on 'label' attribute
     node_labels = {node: G.nodes[node]["label"] for node in G.nodes}
+    fig, ax = plt.subplots(figsize=(8, 6))
     nx.draw(
         G,
         pos,
@@ -153,17 +186,26 @@ def plotObesityGraph(post_data, layout=1):
         node_color=node_colors,
         edge_color="gray",
         font_weight="bold",
+        ax=ax
     )
-    # save the plot with the post title (optional, can change basis for saving)
-    plt.title(splitTitle(post_data["title"]))
-    folder_path = "plot_graphs"
-    os.makedirs(folder_path, exist_ok=True)  # Create the folder if it doesn't exist
-    file_path = os.path.join(folder_path, f"{post_data['submission_id']}_graph.png")
-    # print(file_path)
-    # plt.savefig(file_path)
-    plt.show()
+    # make Tkinter window
+    window = tk.Tk()
+    window.title(splitTitle(post_data["title"]))
+    # embed Matplotlib figure in the Tkinter window
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    # add toolbar window
+    toolbar = ttk.Notebook(window)
+    toolbar.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-def plotFatGraph(post_data, layout=1):
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    # runs an eventloop; listens for actions to happen via window or user interaction
+    window.mainloop()
+    # close matplotlib fig to prevent resource leaks
+    plt.close()
+
+def plotFatGraph(post_data, layout=5):
     '''plot a graph based on a comment tree for a post in r/fatlogic
     '''
     G = nx.DiGraph()
@@ -181,6 +223,28 @@ def plotFatGraph(post_data, layout=1):
     node_sizes = [300 if node == root_id else 30 for node in G.nodes]
     # only label root node w unique post ID
     node_labels = {node: G.nodes[node]["label"] if node == root_id else "" for node in G.nodes}
+    # nx.draw(
+    #     G,
+    #     pos,
+    #     labels=node_labels,
+    #     with_labels=True,
+    #     font_size=8,
+    #     font_color="black",
+    #     node_color=node_colors,
+    #     node_size=node_sizes,
+    #     edge_color="gray",
+    #     font_weight="bold",
+    # )
+
+    # # save the plot with the post title (optional, can change basis for saving)
+    # plt.title(splitTitle(post_data["title"]))
+    # folder_path = "plot_graphs"
+    # os.makedirs(folder_path, exist_ok=True)  # Create the folder if it doesn't exist
+    # file_path = os.path.join(folder_path, f"{post_data['submission_id']}_graph.png")
+    # # print(file_path)
+    # # plt.savefig(file_path)
+    # plt.show()
+    fig, ax = plt.subplots(figsize=(8, 6))
     nx.draw(
         G,
         pos,
@@ -192,16 +256,74 @@ def plotFatGraph(post_data, layout=1):
         node_size=node_sizes,
         edge_color="gray",
         font_weight="bold",
+        ax=ax
     )
+    # make Tkinter window
+    window = tk.Tk()
+    window.title(splitTitle(post_data["title"]))
+    # embed Matplotlib figure in the Tkinter window
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    # add toolbar window
+    toolbar = ttk.Notebook(window)
+    toolbar.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-    # save the plot with the post title (optional, can change basis for saving)
-    plt.title(splitTitle(post_data["title"]))
-    folder_path = "plot_graphs"
-    os.makedirs(folder_path, exist_ok=True)  # Create the folder if it doesn't exist
-    file_path = os.path.join(folder_path, f"{post_data['submission_id']}_graph.png")
-    # print(file_path)
-    # plt.savefig(file_path)
-    plt.show()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    # runs an eventloop; listens for actions to happen via window or user interaction
+    window.mainloop()
+    # close matplotlib fig to prevent resource leaks
+    plt.close()
+
+def plotObesityMany(post_data, ax, layout=5):
+    '''draws plots from a r/obesity comment tree; meant for creating multiple graphs
+    '''
+    G = nx.DiGraph()
+    root_id = post_data["submission_id"]
+    G.add_node(root_id, label=root_id, color="palevioletred")
+    for comment in post_data.get("comments", []):
+        G.add_node(comment["comment_id"], label=convert_timestamp(comment["timestamp"]), color="cadetblue")
+        G.add_edge(root_id, comment["comment_id"])
+        addNodesObesity(G, comment)
+    pos = pickLayout(G, layout)
+    node_colors = [G.nodes[node]["color"] for node in G.nodes]
+    nx.draw(
+        G,
+        pos,
+        font_size=4,
+        node_size=30,
+        font_color="black",
+        node_color=node_colors,
+        edge_color="gray",
+        font_weight="bold",
+        ax=ax
+    )
+    # ax.set_title(splitTitle(post_data["title"]), fontsize=4)
+
+def plotFatMany(post_data, ax, layout=5):
+    '''draws plots from a r/fatlogic comment tree; meant for creating multiple graphs
+    '''
+    G = nx.DiGraph()
+    root_id = post_data["submission_id"]
+    G.add_node(root_id, label=root_id, color="#E4572E")
+    for comment in post_data.get("comments", []):
+        G.add_node(comment["comment_id"], color="#29335C")
+        G.add_edge(root_id, comment["comment_id"])
+        addNodesFat(G, comment)
+    pos = pickLayout(G, layout)
+    node_colors = [G.nodes[node]["color"] for node in G.nodes]
+    nx.draw(
+        G,
+        pos,
+        font_size=8,
+        node_size=30,
+        font_color="black",
+        node_color=node_colors,
+        edge_color="gray",
+        font_weight="bold",
+        ax=ax
+    )
+    ax.set_title(splitTitle(post_data["title"]))
 
 def pickLayout(graph, layout):
     '''returns the chosen graph layout for drawing
@@ -217,10 +339,6 @@ def pickLayout(graph, layout):
     else:
         # default layout
         return nx.planar_layout(graph)
-
-
-# line to test code
-# plotFatGraph(fl_data[0])
 
 # actually run main
 if __name__ == '__main__':
